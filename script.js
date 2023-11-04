7679f6e1825425192d93255e28643d2bd8323c4a
Felix <2000fsommer@gmail.com>
2023-11-04 16:41:18 +0100

    computerchoice index

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const choices = ["rock","paper","scissors"];
+function  getComputerChoice(){
+    let computerChoice = Math.floor(Math.random() * choices.length);
+
+    return computerChoice
+}
+
+console.log(getComputerChoice())
 
