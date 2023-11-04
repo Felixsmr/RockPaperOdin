6a98aa8a63fadf202d3608b6cac4d2d74f5e22b7
Felix <2000fsommer@gmail.com>
2023-11-04 17:29:33 +0100

    playRound checked

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-const choices = ["rock","paper","scissors"];
+const choices = ["Rock","Paper","Scissors"];
 function  getComputerChoice(){
     let computerChoiceIndex = Math.floor(Math.random() * choices.length);
 
-    return choices[computerChoiceIndex]
+    return choices[computerChoiceIndex];
 }
 
-console.log(getComputerChoice())
+function playerPick(){
+    let playerschoice = prompt("Choose: Rock/Paper/Scissors: ")
+
+    return playerschoice
+}
+
+
+function playRound(playerSelection, computerSelection){
+
+    player = playerSelection().toLowerCase()
+    computer = computerSelection().toLowerCase()
+
+    if(player === computer){
+        return `${player} equals ${computer}. It is a Tie!`
+    } else if(player === "rock" && computer == "scissors"){
+        return `${player} beats ${computer}. You won`
+    } else if(player === "paper" && computer == "rock"){
+        return `${player} beats ${computer}. You won`
+    }else if(player === "scissors" && computer == "paper"){
+        return `${player} beats ${computer}. You won`
+    } else{
+        return `${computer} beats ${player}. You lost`
+    }
+    
+}
+
+console.log(playRound(playerPick,getComputerChoice))
+
+
+
+
 
